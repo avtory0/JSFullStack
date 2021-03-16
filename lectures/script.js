ca729c7c6b29b234396b8dd7e6ea17fe3d8589f9
Maksim <easypeasylemonskvii@gmail.com>
2021-03-16 19:56:07 +0300

    first commit

---
+++ lectures/script.js
@@ -0,0 +1,54 @@
+let number = 1;
+const leftBorderWidth = 5;
+
+// number = 10;
+// console.log(number);
+
+// const obj = {
+//     a: 10
+// };
+
+// obj.a = 50;
+// console.log(obj);
+
+//object
+
+// const objj = {
+//     name: "Harry",
+//     age:12,
+//     isGay:false
+// };
+
+// console.log(objj);
+
+// let arr = ['img', 'orange', 2,5];
+// console.log(arr[1]);
+
+
+// const answers = [];
+
+// answers[0]=prompt('how are u?');
+// answers[1]=prompt('how are u?');
+
+//интерполяция
+
+// const category = "toys";
+
+// console.log("http://someurl.com/" + category ); //старый способ
+
+// console.log(`http://someurl.com/${category}` ); //интерполяция
+
+// const user = "Artem";
+// alert(`hello, ${user}`);
+
+
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
