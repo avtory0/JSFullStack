274d6889b1b2da83ce959786bad145e4589eb8cc
Maksim <easypeasylemonskvii@gmail.com>
2021-03-17 15:22:35 +0300

    scnd task

--- lectures/script.js
+++ lectures/script.js
@@ -54,3 +54,56 @@ console.log(--decr);
 console.log(5%2);
 
 //условия №13
+
+//обычные условия
+// const num = 50;
+// if(num <49){
+//     console.log('Слишком мало');
+// }
+// else if(num > 50 ){
+//     console.log("Много");
+// }
+// else {
+//     console.log("OK");
+// } 
+
+// тернарный оператор
+// (num == 50) ? console.log('OK!') : console.log('Error');
+
+// //switch
+// switch(num) {
+//     case 49:
+//         console.log('false');
+//         break;
+//     case 100:
+//         console.log('false again');
+//         break;
+//     case 51:
+//         console.log('its true');
+//         break;
+//     default:
+//         console.log('not today');
+//         break;
+// }
+
+//Циклы №14
+
+let num = 50;
+// while(num<55){
+//     console.log(num);
+//     num++;
+// }
+
+// do{
+//     console.log(num);
+//     num++;
+// }
+// while(num<55);
+
+for (let i =1; i < 8; i++){
+    if (i ==6){
+        break;
+    }
+    console.log(num);
+    num++;
+}
